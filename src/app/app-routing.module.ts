da1e0156bc4c272d5d8f1abe6a9f1eab7a06b30f
morenats <mtorressendra@gmail.com>
2024-04-22 10:49:57 -0300

    creamos los primeros modulos y configuramos las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NombreComponenteComponent } from './nombre-componente/nombre-componente.component';
+import { InicioComponent } from './modules/inicio/pages/inicio/inicio.component';
 
 const routes: Routes = [{path:"nombre_componente",component:NombreComponenteComponent},
-{path:"",component:NombreComponenteComponent}];
-
+{path:"",component:NombreComponenteComponent},
+{
+  path: "",component: InicioComponent
+},
+{
+  path:"",loadChildren:()=>import('./modules/inicio/inicio.module').then(m=>m.InicioModule)
+}
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
