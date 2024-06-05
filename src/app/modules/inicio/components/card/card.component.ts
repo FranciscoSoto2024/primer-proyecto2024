d7ed8f58854b6f8f288a0d6840d33e39afeed2eb
morenats <mtorressendra@gmail.com>
2024-06-05 08:55:48 -0300

    avance en las card component

--- src/app/modules/inicio/components/card/card.component.ts
+++ src/app/modules/inicio/components/card/card.component.ts
@@ -28,7 +28,16 @@ export class CardComponent {
       edad: 1500,
       imagen: "https://th.bing.com/th/id/OIP.A5A0INeZMXE7W3PdIoNQawAAAA?rs=1&pid=ImgDetMain",
       descripcion:"El estilo old school tiene sus raíces en los primeros años del siglo XX en los Estados Unidos. Su origen se remonta a los marineros que se tatuaban durante sus viajes por todo el mundo. Estos tatuajes, realizados en su mayoría en puertos y ciudades portuarias, se caracterizaban por sus diseños audaces, colores vibrantes y líneas gruesas. Los motivos clásicos del old school incluyen anclas, corazones, golondrinas, rosas, sirenas y otros elementos marítimos"
-    }  
+    },
+    {
+      id: "",
+      nombre: "tribal",
+      origen: "tribus polineseas",
+      estilo: "tribal",
+      edad: 5000,
+      imagen: "https://t1.uc.ltmcdn.com/es/posts/7/3/4/_49437_1_600.jpg",
+      descripcion:"Los tatuajes tribales son diseños que se caracterizan por su simplicidad y líneas geométricas. A menudo, se inspiran en patrones tradicionales de diferentes culturas y se aplican en áreas específicas del cuerpo. Estos tatuajes han sido una forma popular de arte corporal durante siglos, y aunque el significado puede variar dependiendo de la cultura y la ubicación, todos comparten un aspecto en común: son un símbolo de identidad y pertenencia."
+    },
   ]
    }
 }
